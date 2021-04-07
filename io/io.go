6021c40f7aaae056bdec1f06135076f651627dad
smart <smart>
2021-04-07 10:14:56 +0800

    commit some code

--- io/io.go
+++ io/io.go
@@ -1,6 +1,11 @@
 package io
 
-import "io"
+import (
+	"bytes"
+	"io"
+
+	"github.com/pkg/errors"
+)
 
 type Reader interface {
 	io.Reader
@@ -12,3 +17,31 @@ type Writer interface {
 	io.Writer
 	WriteFull([]byte) error
 }
+
+type buffer struct {
+	*bytes.Buffer
+}
+
+func NewReader(buf []byte) (reader Reader) {
+	return buffer{Buffer: bytes.NewBuffer(buf)}
+}
+
+func (b buffer) ReadFull(buf []byte) error {
+	n, err := io.ReadFull(b.Buffer, buf)
+	if err != nil {
+		return errors.Wrap(err, "io.ReadFull")
+	}
+	if n != len(buf) {
+		return errors.New("do not read enough data")
+	}
+	return nil
+}
+
+func (b buffer) ReadN(n int) (buf []byte, err error) {
+	buf = make([]byte, n)
+	err = b.ReadFull(buf)
+	return buf, err
+}
+
+//read dump
+//write dump
