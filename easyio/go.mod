1621b8e310e423e3f0510c94ae318ba1869ed11e
SmartBrave <SmartBraveCoder@gmail.com>
2021-04-10 20:39:49 +0800

    rename io to easyio

---
+++ easyio/go.mod
@@ -0,0 +1,3 @@
+module github.com/SmartBrave/utils/easyio
+
+go 1.16
---
+++ easyio/io.go
@@ -0,0 +1,59 @@
+package easyio
+
+import (
+	"io"
+)
+
+type Reader interface {
+	io.Reader
+	ReadFull(b []byte) error
+	ReadN(int) ([]byte, error)
+}
+
+type easyReader struct {
+	io.Reader
+}
+
+func NewReader(r io.Reader) (reader Reader) {
+	return easyReader{Reader: r}
+}
+
+func (er easyReader) ReadFull(buf []byte) error {
+	n, err := io.ReadFull(er, buf)
+	if err != nil {
+		return err
+	}
+	if n != len(buf) {
+		return err
+	}
+	return nil
+}
+
+func (er easyReader) ReadN(n int) (buf []byte, err error) {
+	buf = make([]byte, n)
+	err = er.ReadFull(buf)
+	return buf, err
+}
+
+type Writer interface {
+	io.Writer
+	// WriteFull([]byte) error
+	// WriteN([]byte, int) error
+}
+
+type easyWriter struct {
+	io.Writer
+}
+
+func NewWriter(w io.Writer) (writer Writer) {
+	return easyWriter{Writer: w}
+}
+
+// func (ew easyWriter) WriteFull(buf []byte) (err error) {
+// }
+
+// func (ew easyWriter) WriteN(buf []byte, n int) (err error) {
+// }
+
+//read dump
+//write dump
