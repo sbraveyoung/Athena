69aa81c228fea5a6fabe0425afee5551398313ce
smart <SmartBraveCoder@gmail.com>
2021-06-16 10:46:05 +0800

    update code

--- easyio/io.go
+++ easyio/io.go
@@ -4,17 +4,18 @@ import (
 	"io"
 )
 
-type Reader interface {
+type EasyReader interface {
 	io.Reader
 	ReadFull(b []byte) error
 	ReadN(int) ([]byte, error)
+	ReadAll() ([]byte, error)
 }
 
 type easyReader struct {
 	io.Reader
 }
 
-func NewEasyReader(r io.Reader) (reader Reader) {
+func NewEasyReader(r io.Reader) (reader EasyReader) {
 	return easyReader{Reader: r}
 }
 
@@ -32,9 +33,13 @@ func (er easyReader) ReadN(n int) (buf []byte, err error) {
 	return buf, err
 }
 
-type Writer interface {
+func (er easyReader) ReadAll() (buf []byte, err error) {
+	return io.ReadAll(er)
+}
+
+type EasyWriter interface {
 	io.Writer
-	// WriteFull([]byte) error
+	WriteFull([]byte) error
 	// WriteN([]byte, int) error
 }
 
@@ -42,20 +47,36 @@ type easyWriter struct {
 	io.Writer
 }
 
-func NewEasyWriter(w io.Writer) (writer Writer) {
+func NewEasyWriter(w io.Writer) (writer EasyWriter) {
 	return easyWriter{Writer: w}
 }
 
-// func (ew easyWriter) WriteFull(buf []byte) (err error) {
-// }
+func (ew easyWriter) WriteFull(buf []byte) error {
+	_, err := ew.Write(buf)
+	return err
+}
 
-// func (ew easyWriter) WriteN(buf []byte, n int) (err error) {
-// }
+type EasyReadWriter interface {
+	EasyReader
+	EasyWriter
+}
+
+type easyReadWriter struct {
+	EasyReader
+	EasyWriter
+}
+
+func NewEasyReadWriter(rw io.ReadWriter) (readWriter EasyReadWriter) {
+	return easyReadWriter{
+		EasyReader: NewEasyReader(rw),
+		EasyWriter: NewEasyWriter(rw),
+	}
+}
 
 //read dump
 //write dump
 
-func CopyFull(dst easyWriter, src easyReader) (err error) {
+func CopyFull(dst EasyWriter, src EasyReader) (err error) {
 	_, err = io.Copy(dst, src)
 	return err
 }
