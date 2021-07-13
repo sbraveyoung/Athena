7fadf9f7b19ee0ef266153e75c7fdbfbd771edc1
smart <SmartBraveCoder@gmail.com>
2021-07-13 20:24:51 +0800

    add Flush method to Writer

--- easyio/io.go
+++ easyio/io.go
@@ -1,7 +1,10 @@
 package easyio
 
 import (
+	"bufio"
 	"io"
+
+	"github.com/pkg/errors"
 )
 
 type EasyReader interface {
@@ -19,41 +22,56 @@ func NewEasyReader(r io.Reader) (reader EasyReader) {
 	return easyReader{Reader: r}
 }
 
-func (er easyReader) ReadFull(buf []byte) error {
-	_, err := io.ReadFull(er, buf)
-	if err != nil {
+func (er easyReader) ReadFull(b []byte) (err error) {
+	var n int
+	n, err = io.ReadFull(er, b)
+	if err == io.EOF {
 		return err
+	}
+	if err != nil {
+		return errors.Wrap(err, "rtmp.conn.Read")
+	}
+	if n != len(b) {
+		return errors.New("do not read enough data from conn")
 	}
 	return nil
 }
 
-func (er easyReader) ReadN(n uint32) (buf []byte, err error) {
-	buf = make([]byte, n)
-	err = er.ReadFull(buf)
-	return buf, err
+func (er easyReader) ReadN(n uint32) (b []byte, err error) {
+	b = make([]byte, n)
+	err = er.ReadFull(b)
+	return b, err
 }
 
-func (er easyReader) ReadAll() (buf []byte, err error) {
+func (er easyReader) ReadAll() (b []byte, err error) {
 	return io.ReadAll(er)
 }
 
 type EasyWriter interface {
 	io.Writer
 	WriteFull([]byte) error
+	Flush() error
 	// WriteN([]byte, int) error
 }
 
 type easyWriter struct {
-	io.Writer
+	// io.Writer
+	*bufio.Writer
 }
 
 func NewEasyWriter(w io.Writer) (writer EasyWriter) {
-	return easyWriter{Writer: w}
+	return easyWriter{Writer: bufio.NewWriter(w)}
 }
 
-func (ew easyWriter) WriteFull(buf []byte) error {
-	_, err := ew.Write(buf)
-	return err
+func (ew easyWriter) WriteFull(b []byte) error {
+	n, err := ew.Write(b)
+	if err != nil {
+		return errors.Wrap(err, "conn.Write")
+	}
+	if n != len(b) {
+		return errors.New("do not write enough data to conn")
+	}
+	return nil
 }
 
 type EasyReadWriter interface {
